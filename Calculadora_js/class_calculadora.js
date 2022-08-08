2df9660544ab4229f18316c45d04815b6102c50a
samuca23 <samuelchiodini@hotmail.com>
2022-08-08 00:39:40 -0300

    documentacao e estilo

--- Calculadora_js/class_calculadora.js
+++ Calculadora_js/class_calculadora.js
@@ -10,25 +10,17 @@ class Calculadora {
     montaLayotCalculadora = function () {
         var iPulaLinha = 0;
         var oElementContainer = document.getElementsByClassName('container');
+        var oElementoCalculadora = this.montaDivCalculadora();
+        var oElementoDivInput = this.montaDivVisor();
+        var oElementoDivNumber = this.montaDivNumber();
 
-        var oElementoCalculadora = document.createElement('div');
-        oElementoCalculadora.setAttribute('class', 'calculadora');
-        oElementoCalculadora.style.width = '241px';
-        oElementoCalculadora.style.height = '240px';
-        oElementoCalculadora.style.backgroundColor = 'black';
-
-        var oElementoDivInput = document.createElement('div');
-        oElementoDivInput.setAttribute('class', 'visor');
-
-        var oElementoDivNumber = document.createElement('div', 'number');
-        oElementoDivNumber.setAttribute('class', 'number');
-        oElementoDivNumber.style.padding = '5px';
-
+        /* Adiciona os Elementos para a Tela */
         oElementContainer[0].appendChild(oElementoCalculadora);
         oElementoCalculadora.appendChild(oElementoDivInput);
-        oElementoDivInput.appendChild(this.montaInputCalc());
+        oElementoDivInput.appendChild(this.montaVisorCalc());
         oElementoCalculadora.appendChild(oElementoDivNumber);
 
+        /* Adicionando os botões para a Tela */
         var aButtonNumber = this.montaButtonNumberCalc();
         aButtonNumber.forEach(function (oButtonNumber) {
 
@@ -43,7 +35,7 @@ class Calculadora {
             oElementoDivNumber.appendChild(oButtonNumber);
         });
 
-        /* Adicionar os botoes de operaçao */
+        /* Adicionar os botões de operação */
         var aButtonOperator = this.montaButtonOperatorCalc();
         aButtonOperator.forEach(function (oButtonOperator) {
             oElementoDivNumber.appendChild(oButtonOperator);
@@ -51,15 +43,52 @@ class Calculadora {
 
         oElementoDivNumber.appendChild(this.montaButtonIgual());
         oElementoDivNumber.appendChild(this.montaButtonLimpa());
-
     }
 
     /**
-     * Monta o Input que será a Tela da Calculadora.
+     * Cria a Div da Calculadora.
      * 
-     * @returns 
+     * @returns Element HTML
      */
-    montaInputCalc = function () {
+    montaDivCalculadora = function () {
+        var oElementoCalculadora = document.createElement('div');
+        oElementoCalculadora.setAttribute('class', 'calculadora');
+        this.styleCalculadora(oElementoCalculadora);
+
+        return oElementoCalculadora;
+    }
+
+    /**
+     * Cria a Div do visor.
+     * 
+     * @returns Element HTML
+     */
+    montaDivVisor = function () {
+        var oElementoDivInput = document.createElement('div');
+        oElementoDivInput.setAttribute('class', 'visor');
+
+        return oElementoDivInput;
+    }
+
+    /**
+     * Cria a Div dos Números.
+     * 
+     * @returns Element HTML
+     */
+    montaDivNumber = function () {
+        var oElementoDivNumber = document.createElement('div', 'number');
+        oElementoDivNumber.setAttribute('class', 'number');
+        oElementoDivNumber.style.padding = '5px';
+
+        return oElementoDivNumber;
+    }
+
+    /**
+     * Cria o Input que será a Tela da Calculadora.
+     * 
+     * @returns Element HTML
+     */
+    montaVisorCalc = function () {
         var oElementInput = document.createElement("input");
         oElementInput.setAttribute("type", "text");
         oElementInput.setAttribute("class", "input-visor");
@@ -70,13 +99,18 @@ class Calculadora {
         return oElementInput;
     }
 
+    /**
+     * Retorna o Visor.
+     * 
+     * @returns Element HTML
+     */
     getInputCalc = function () {
         return document.querySelector('.input-visor');
     }
 
 
     /**
-     * Monta os botões numéricos.
+     * Cria os botões numéricos.
      * 
      * @returns Array
      */
@@ -93,11 +127,8 @@ class Calculadora {
                 var oInputCalc = document.querySelector('.input-visor');
                 oInputCalc.value += this.innerText;
             });
-
             /* Estilo dos botões */
-            oElementButton.style.width = '75px';
-            oElementButton.style.margin = '1px';
-
+            this.stylePadraoButtonNumber(oElementButton);
             aRetorno.push(oElementButton);
         }
 
@@ -105,7 +136,7 @@ class Calculadora {
     }
 
     /**
-     * Monta os operadores passados por parâmetro.
+     * Cria os operadores passados por parâmetro.
      * 
      * @param {Array} aOperador 
      * @returns Array
@@ -117,19 +148,26 @@ class Calculadora {
         aOperador.forEach(function (oOperador) {
             var oButtonOperator = document.createElement('button');
             oButtonOperator.setAttribute('class', 'btn btn-danger');
+            oButtonOperator.style.width = '36px';
+            oButtonOperator.style.margin = '1px';
+
             oButtonOperator.appendChild(document.createTextNode(oOperador));
 
             oButtonOperator.addEventListener('click', function () {
                 var oInputCalc = document.querySelector('.input-visor');
                 oInputCalc.value += this.innerText;
             });
-
             aButtonOperator.push(oButtonOperator);
         });
 
         return aButtonOperator;
     }
 
+    /**
+     * Cria o botão de Igual.
+     * 
+     * @returns Element HMTL
+     */
     montaButtonIgual = function () {
         var oElementButtonIgual = document.createElement("button");
         oElementButtonIgual.appendChild(document.createTextNode('='));
@@ -138,6 +176,7 @@ class Calculadora {
         oElementButtonIgual.addEventListener('click', function () {
             var oInputCalc = document.querySelector('.input-visor');
             var sConta = oInputCalc.value;
+
             if (sConta) {
                 var iResultado = eval(sConta);
                 oInputCalc.value = iResultado;
@@ -148,10 +187,16 @@ class Calculadora {
         return oElementButtonIgual;
     }
 
+    /**
+     * Cria o Botão de Limpar o Visor.
+     * 
+     * @returns Element HTML
+     */
     montaButtonLimpa = function () {
         var oElementButtonLimpar = document.createElement("button");
         oElementButtonLimpar.appendChild(document.createTextNode("C"));
         oElementButtonLimpar.setAttribute('class', 'btn btn-primary');
+
         oElementButtonLimpar.addEventListener('click', function () {
             var oInputCalc = document.querySelector('.input-visor');
             oInputCalc.value = "";
@@ -160,6 +205,26 @@ class Calculadora {
         return oElementButtonLimpar;
     }
 
+    /**
+     * Estilo para a Calculadora
+     * 
+     * @param {Elemnt} oCalc 
+     */
+    styleCalculadora = function (oCalc) {
+        oCalc.style.width = '241px';
+        oCalc.style.height = '241px';
+        oCalc.style.backgroundColor = 'black';
+    }
+
+    /**
+     * Estilo padrão para os botões numéricos.
+     * 
+     * @param {Element} oButton 
+     */
+    stylePadraoButtonNumber = function (oButton) {
+        oButton.style.width = '75px';
+        oButton.style.margin = '1px';
+    }
 }
 
 let calculadora = new Calculadora();
