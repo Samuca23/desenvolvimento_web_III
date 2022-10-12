45fcf0c6ed6c5ca26d2995f60401765d2dcd6211
samuca23 <samuelchiodini@hotmail.com>
2022-10-12 11:01:44 -0300

    video 11

--- Atv_video/client/app/controllers/NegociacaoController.js
+++ Atv_video/client/app/controllers/NegociacaoController.js
@@ -12,29 +12,20 @@ class NegociacaoConttroller {
     adiciona(event) {
         event.preventDefault();
 
-        let converter = new DateConverter();
-
-        const data = DateConverter.paraData(this_._inputData.value);
-        console.log(data);
-
-        let data = new Date(...this._inputData.value.split('-').map((item, indice) => item - (indice % 2)));
-
-        this._negociacoes.adiciona(negociacao);
-
-        console.log(this._negociacoes.paraArray());
-
+        this._negociacoes.adiciona(this._criaNegociacao());
         this._limparFormulario();
     }
 
     _limparFormulario() {
-        this._inputData.val = '2022-01-03';
-        this._inputQuantidade.val = 1;
-        this._inputValor.val = 2.0
+        this._inputData.value = '2022-01-03';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 2.0
         this._inputData.focus();
     }
 
     _criaNegociacao() {
-        return new Negociacao(data,
+        return new Negociacao(
+            DateConverter.paraData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value);
     }
